50b5f2c5eee902759a6430f7732e70da0f9f6efe
iyaoxm <>
2023-03-09 18:06:43 +0800

    bind

--- admin/service/dto/user.go
+++ admin/service/dto/user.go
@@ -1,26 +1,18 @@
 package dto
 
 import (
-	"github.com/iyaoo/go-IMChat/common/models"
+	"github.com/CoolBank/coinhub-base/logger"
+	"github.com/gin-gonic/gin"
 )
 
 // UserControl 用户增、改使用结构体
 type UserControl struct {
-	ID   int    `json:"id" gorm:"column:id;primaryKey;autoIncrement;comment:自增主键id"`
-	Name string `json:"name" gorm:"column:name;not null;comment:用户姓名"`
-	//PassWord      string    `json:"password" gorm:"column:password;not null;comment:用户密码"`
-	Gender        string `json:"gender" gorm:"column:gender"`
-	Phone         string `json:"phone" gorm:"column:phone"`
-	Email         string `json:"email" gorm:"column:email"`
-	Identity      string `json:"identity" gorm:"column:identity"`
-	ClientIP      string `json:"client_ip" gorm:"column:client_ip"`
-	ClientPost    string `json:"client_post" gorm:"column:client_post"`
-	IsLogin       bool   `json:"is_login" gorm:"column:is_login"`
-	HeartBeatTime string `json:"heartbeat_time" gorm:"column:heartbeat_time"`
-	DeviceInfo    string `json:"device_info" gorm:"column:device_info"`
-	//LoginTime     time.Time `json:"login_time" gorm:"column:login_time"`
-	//LogoutTime    time.Time `json:"logout_time" gorm:"column:logout_time"`
-	models.GormTime
+	Name     string `json:"name" gorm:"column:name;not null;comment:用户姓名"`
+	PassWord string `json:"password" gorm:"column:password;not null;comment:用户密码"`
+	Gender   string `json:"gender" gorm:"column:gender"`
+	Phone    string `json:"phone" gorm:"column:phone"`
+	Email    string `json:"email" gorm:"column:email"`
+	Identity string `json:"identity" gorm:"column:identity"`
 }
 
 // TableName 获取表名
@@ -28,11 +20,36 @@ func (table *UserControl) TableName() string {
 	return "user"
 }
 
+// Bind 映射上下文中的结构体数据
+func (s *UserControl) Bind(ctx *gin.Context) error {
+	err := ctx.Bind(s)
+	if err != nil {
+		logger.Debugf("Bind error: %s", err.Error())
+		return err
+	}
+	return err
+}
+
 // UserById 获取单个或者删除的结构体
 type UserById struct {
 	UserID int `uri:"id"`
 }
 
-func (u *UserById) GetId() interface{} {
-	return u.UserID
+// GetId
+func (s *UserById) GetId() interface{} {
+	return s.UserID
+}
+
+// Bind
+func (s *UserById) Bind(ctx *gin.Context) error {
+	err := ctx.ShouldBindUri(s)
+	if err != nil {
+		logger.Debugf("ShouldBindUri error: %s", err.Error())
+		return err
+	}
+	err = ctx.ShouldBind(s)
+	if err != nil {
+		logger.Debugf("ShouldBind error: %s", err.Error())
+	}
+	return err
 }
